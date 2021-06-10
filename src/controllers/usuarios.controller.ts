a1fa738c1d34e961f7f74ff4d7de54610d005700
lucasmartin007 <luesma102938475@hotmail.com>
2021-06-10 19:40:53 -0300

    Registro de usuario

--- src/controllers/usuarios.controller.ts
+++ src/controllers/usuarios.controller.ts
@@ -2,7 +2,110 @@
 
 // import {inject} from '@loopback/core';
 
+import {
+  repository
+} from '@loopback/repository';
+import {
+  getModelSchemaRef,
+
+
+
+
+
+
+  post,
+
+
+
+
+
+
+
+
+  requestBody,
+  response,
+
+  SchemaObject
+} from '@loopback/rest';
+import {Usuarios} from '../models/Usuarios.model';
+import {UsuariosRepository} from '../repositories';
+
+
+const CredentialsSchema: SchemaObject = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    password: {
+      type: 'string',
+      minLength: 8,
+    },
+  },
+};
+
+export const CredentialsRequestBody = {
+  description: 'The input of login function',
+  required: true,
+  content: {
+    'application/json': {schema: CredentialsSchema},
+  },
+};
 
 export class UsuariosController {
-  constructor() {}
+  constructor(
+    @repository(UsuariosRepository)
+    public usuariosRepository : UsuariosRepository,
+
+  ) {}
+
+  @post('/usuarios')
+  @response(200, {
+    description: 'Usuarios model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Usuarios)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Usuarios, {
+            title: 'NewUsuarios',
+
+          }),
+        },
+      },
+    })
+    usuarios: Usuarios,
+  ): Promise<Usuarios> {
+    return this.usuariosRepository.create(usuarios);
+  }
+
+  //
+
+  // @get('/greet/{name}', {
+  //   responses: {
+  //     '200': {
+  //       description: '',
+  //       content: {
+  //         'application/json': {
+  //           schema: {
+  //             type: 'object',
+  //             properties: {
+  //               message: 'string',
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // })
+  // async greet(@param.path.string('name') name: string): Promise<Object> {
+  //   const greeting = "Hola " + name
+  //   return {
+  //     timestamp: new Date(),
+  //     greeting,
+  //   };
+  // }
 }

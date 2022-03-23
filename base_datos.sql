7bd8abb7107fe0b19857b41a1f2a86b2ad58bda9
lucasmartin007 <luesma102938475@hotmail.com>
2022-03-23 20:09:43 -0300

    token

--- base_datos.sql
+++ base_datos.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 08-08-2021 a las 18:53:03
--- Versión del servidor: 8.0.26-0ubuntu0.20.04.2
--- Versión de PHP: 7.4.3
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-03-2022 a las 23:32:49
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,25 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `proy_mensajeria`
+-- Base de datos: `proy-mensajeria`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Messages`
---
-
-CREATE TABLE `Messages` (
-  `id` int NOT NULL,
-  `sender` int NOT NULL,
-  `receiver` int NOT NULL,
-  `message` varchar(512) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `es_archivo` tinyint(1) NOT NULL,
-  `nombre_archivo` varchar(512) DEFAULT NULL,
-  `cont_imagen` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--____ACLARACION____: LOS NOMBRES DE LAS TABLAS DEBEN CAMBIARSE A MAYUSCULAS EN LA PRIMERA LETRA EN EL CASO DE USAR LINUX O MAC
 
 -- --------------------------------------------------------
 
@@ -46,51 +30,53 @@ CREATE TABLE `Messages` (
 --
 
 CREATE TABLE `messages` (
-  `id` int NOT NULL,
-  `sender` int NOT NULL,
-  `receiver` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
   `message` varchar(512) NOT NULL,
   `created_at` datetime NOT NULL,
   `es_archivo` tinyint(1) NOT NULL,
-  `nombre_archivo` varchar(512) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `nombre_archivo` varchar(512) DEFAULT NULL,
+  `cont_imagen` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `messages`
 --
 
-INSERT INTO `messages` (`id`, `sender`, `receiver`, `message`, `created_at`, `es_archivo`, `nombre_archivo`) VALUES
-(1, 1, 2, 'Hola', '2021-06-29 01:46:13', 0, NULL),
-(2, 1, 2, 'Como estas?', '2021-06-29 01:47:15', 0, NULL),
-(3, 2, 1, 'Hola. Bien, muy bien', '2021-06-29 02:44:51', 0, NULL),
-(4, 1, 3, 'Hola', '2021-06-29 02:46:55', 0, NULL),
-(5, 1, 3, 'Todo bien?', '2021-06-29 02:53:48', 0, NULL),
-(6, 3, 1, 'Hola! Bien', '2021-06-29 02:59:30', 0, NULL),
-(7, 1, 3, 'Que contas?', '2021-06-29 14:46:17', 0, NULL),
-(8, 1, 2, 'Que estas haciendo?', '2021-06-29 14:46:25', 0, NULL),
-(9, 2, 1, 'Nada, leo un libro. Vos?', '2021-06-29 14:55:52', 0, NULL),
-(10, 3, 1, 'Nada, aca tranqui. Escuchando musica, vos?', '2021-06-29 14:56:17', 0, NULL),
-(11, 1, 2, 'Viendo unos videos de Youtube', '2021-06-29 14:56:47', 0, NULL),
-(12, 1, 3, 'Nada, leyendo noticias', '2021-06-29 14:57:22', 0, NULL),
-(64, 1, 2, 'capac-ith-2-junio.txt', '2021-07-03 01:28:34', 1, 'capac-ith-2-junio.txt');
+INSERT INTO `messages` (`id`, `sender`, `receiver`, `message`, `created_at`, `es_archivo`, `nombre_archivo`, `cont_imagen`) VALUES
+(1, 1, 2, 'Hola', '2021-06-29 01:46:13', 0, NULL, NULL),
+(2, 1, 2, 'Como estas?', '2021-06-29 01:47:15', 0, NULL, NULL),
+(3, 2, 1, 'Hola. Bien, muy bien', '2021-06-29 02:44:51', 0, NULL, NULL),
+(4, 1, 3, 'Hola', '2021-06-29 02:46:55', 0, NULL, NULL),
+(5, 1, 3, 'Todo bien?', '2021-06-29 02:53:48', 0, NULL, NULL),
+(6, 3, 1, 'Hola! Bien', '2021-06-29 02:59:30', 0, NULL, NULL),
+(7, 1, 3, 'Que contas?', '2021-06-29 14:46:17', 0, NULL, NULL),
+(8, 1, 2, 'Que estas haciendo?', '2021-06-29 14:46:25', 0, NULL, NULL),
+(9, 2, 1, 'Nada, leo un libro. Vos?', '2021-06-29 14:55:52', 0, NULL, NULL),
+(10, 3, 1, 'Nada, aca tranqui. Escuchando musica, vos?', '2021-06-29 14:56:17', 0, NULL, NULL),
+(11, 1, 2, 'Viendo unos videos de Youtube', '2021-06-29 14:56:47', 0, NULL, NULL),
+(12, 1, 3, 'Nada, leyendo noticias', '2021-06-29 14:57:22', 0, NULL, NULL),
+(19, 1, 2, 'documento-texto.txt', '2022-03-23 21:48:56', 1, 'documento-texto.txt', NULL),
+(20, 1, 2, 'Montaña00.jpg', '2022-03-23 21:49:25', 1, 'Montaña00.jpg', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Solicitudes`
+-- Estructura de tabla para la tabla `solicitudes`
 --
 
-CREATE TABLE `Solicitudes` (
-  `id` int NOT NULL,
+CREATE TABLE `solicitudes` (
+  `id` int(11) NOT NULL,
   `direccion_ip` varchar(512) NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `Solicitudes`
+-- Volcado de datos para la tabla `solicitudes`
 --
 
-INSERT INTO `Solicitudes` (`id`, `direccion_ip`, `created_at`) VALUES
+INSERT INTO `solicitudes` (`id`, `direccion_ip`, `created_at`) VALUES
 (28, '192.168.100.13', '2021-08-07 01:43:58'),
 (29, '192.168.100.13', '2021-08-07 01:47:04'),
 (30, '192.168.100.13', '2021-08-07 01:48:47'),
@@ -106,7 +92,7 @@ CREATE TABLE `user` (
   `id` varchar(255) DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `user`
@@ -124,10 +110,10 @@ INSERT INTO `user` (`id`, `username`, `email`) VALUES
 --
 
 CREATE TABLE `usercredentials` (
-  `id` int DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `userId` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usercredentials`
@@ -141,37 +127,16 @@ INSERT INTO `usercredentials` (`id`, `password`, `userId`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Usuarios`
---
-
-CREATE TABLE `Usuarios` (
-  `id` int NOT NULL,
-  `username` varchar(512) NOT NULL,
-  `password` varchar(512) NOT NULL,
-  `email` varchar(512) NOT NULL,
-  `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `Usuarios`
---
-
-INSERT INTO `Usuarios` (`id`, `username`, `password`, `email`, `created_at`) VALUES
-(1, 'erica', 'erica100', 'erica@gmail.com', '2021-08-07 01:52:52');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(512) NOT NULL,
   `password` varchar(512) NOT NULL,
   `email` varchar(512) NOT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -187,27 +152,15 @@ INSERT INTO `usuarios` (`id`, `username`, `password`, `email`, `created_at`) VAL
 --
 
 --
--- Indices de la tabla `Messages`
---
-ALTER TABLE `Messages`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `Solicitudes`
+-- Indices de la tabla `solicitudes`
 --
-ALTER TABLE `Solicitudes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
+ALTER TABLE `solicitudes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -221,34 +174,22 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `Messages`
---
-ALTER TABLE `Messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT de la tabla `Solicitudes`
+-- AUTO_INCREMENT de la tabla `solicitudes`
 --
-ALTER TABLE `Solicitudes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-
---
--- AUTO_INCREMENT de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `solicitudes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
